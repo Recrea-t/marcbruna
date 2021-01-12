3e9e3eb278597b952cff4e3fffb7dfffff5fe30d
David Gay i Tello <david.gay.tello@protonmail.com>
2021-01-12 09:25:40 +0100

    show current year

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ const StickyFooter = ({title, author}) => {
 		<footer className="footer bg-secondary">
 			<Container fluid>
 				<div className="credits text-center text-white">
-					<span className="copyright">&copy; 2020 {title} | DISSENY: {author.name}</span>
+					<span className="copyright">&copy; {new Date().getFullYear()} {title} | DISSENY: {author.name}</span>
 				</div>
 			</Container>
 		</footer>
