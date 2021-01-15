6146367206b2e3afbe67d93ea1c98ec70c98e555
David Gay i Tello <david.gay.tello@protonmail.com>
2021-01-15 18:16:13 +0100

    custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import Layout from '../components/layout'
+import {Container, Row, Col} from "react-bootstrap"
 
-const NotFoundPage = () => (
-	<Layout>
-		<div>
-			<h1>NOT FOUND</h1>
-			<p>You just hit a route that doesn&#39;t exist...</p>
-		</div>
-	</Layout>
-)
+const NotFoundPage = ({location, data}) => {
+	//const pages = data.allSitePage.nodes.map(({path}) => path)
+
+	return (
+		<Layout>
+			<Container className="not-found-container">
+				<Row className="py-3">
+					<Col className="col-12">
+						<h2>Pàgina no trobada</h2>
+					</Col>
+				</Row>
+			</Container>
+		</Layout >
+	)
+}
 
 export default NotFoundPage
+
+export const query = graphql`
+	query NotFoundPageQuery {
+		allSitePage(filter: {path: {nin: ["/dev-404-page", "/404", "/404.html"]}}) {
+			nodes {
+				path
+			}
+		}
+	}
+`
