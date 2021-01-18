608448fc08ebc6ed8ea19b5da8ec1ab09184c49f
David Gay i Tello <david.gay.tello@protonmail.com>
2021-01-18 14:52:25 +0100

    link to cookies policy in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from "gatsby"
 import {Container} from "react-bootstrap"
 
 const StickyFooter = ({title, author}) => {
@@ -6,7 +7,9 @@ const StickyFooter = ({title, author}) => {
 		<footer className="footer bg-secondary">
 			<Container fluid>
 				<div className="credits text-center text-white">
-					<span className="copyright">&copy; {new Date().getFullYear()} {title} | DISSENY: <a href={author.url} target="_blank" rel="noreferrer">{author.name}</a></span>
+					<span className="copyright">
+						&copy; {new Date().getFullYear()} {title} | <Link to="/politica-de-galetes" title="Política de cookies">Política de galetes</Link> | DISSENY: <a href={author.url} target="_blank" rel="noreferrer">{author.name}</a>
+					</span>
 				</div>
 			</Container>
 		</footer>
