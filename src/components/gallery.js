83325f09f323a2042a720f14a28938a07b14b42d
David Gay i Tello <david.gay.tello@protonmail.com>
2021-02-18 19:43:49 +0100

    fixing products in index

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,19 +1,21 @@
 import React from "react";
 
-import { Row, Col, Image } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const Gallery = ({ name, images }) => {
   const [imageSelected, setImageSelected] = React.useState(images[0]);
+  const sources = getImage(imageSelected).images.sources;
 
   return (
     <Row className="row-cols-1 row-cols-sm-2 my-3 g-0">
       <Col className="col-sm-8 text-center" style={{ height: "75vh" }}>
-        <Image
-          className="preview"
-          {...getImage(imageSelected).images.sources[0]}
-          alt={`Imatge ${name} ampliada`}
-        />
+        <picture>
+          {sources.map((source, index) => (
+            <source key={index} {...source} />
+          ))}
+          <img className="preview" alt={`Imatge ${name} ampliada`} />
+        </picture>
       </Col>
       <Col className="col-sm-4 pt-3 py-sm-0 px-sm-3">
         <div className="gallery">
